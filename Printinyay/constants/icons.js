89c20fffab3e73db6ff4850f0cb48d4264df3e14
Said Rizky Abizard <65222567+SRAbizard@users.noreply.github.com>
2021-05-17 02:09:14 +0700

    Tubes

---
+++ Printinyay/constants/icons.js
@@ -0,0 +1,41 @@
+export const order = require("../assets/icons/order.png");
+export const delivery = require("../assets/icons/delivery.png");
+export const promo = require("../assets/icons/promo.png");
+export const terbaru = require("../assets/icons/terbaru.png");
+export const favorite = require("../assets/icons/favorite.png");
+export const back = require("../assets/icons/back.png");
+export const print = require("../assets/icons/print.png");
+export const like = require("../assets/icons/like.png");
+export const list = require("../assets/icons/list.png");
+export const location = require("../assets/icons/location.png");
+export const master_card = require("../assets/icons/mastercard.png");
+export const nearby = require("../assets/icons/nearby.png");
+export const pin = require("../assets/icons/pin.png");
+export const red_pin = require("../assets/icons/red-pin.png");
+export const search = require("../assets/icons/search.png");
+export const basket = require("../assets/icons/shopping-basket.png");
+export const star = require("../assets/icons/star.png");
+export const sushi = require("../assets/icons/map.png");
+export const user = require("../assets/icons/user.png");
+
+export default {
+  order,
+  delivery,
+  promo,
+  terbaru,
+  favorite,
+  back,
+  print,
+  like,
+  list,
+  location,
+  master_card,
+  nearby,
+  pin,
+  red_pin,
+  search,
+  basket,
+  star,
+  sushi,
+  user,
+};
---
+++ Printinyay/constants/images.js
@@ -0,0 +1,33 @@
+const avatar_1 = require("../assets/images/avatar-1.jpg");
+const avatar_2 = require("../assets/images/avatar-2.jpg");
+const avatar_3 = require("../assets/images/avatar-3.jpg");
+const avatar_4 = require("../assets/images/avatar-4.jpg");
+const avatar_5 = require("../assets/images/avatar-5.jpg");
+const fotocopy1 = require("../assets/images/fotocopy1.jpg");
+const fotocopy2 = require("../assets/images/fotocopy2.png");
+const fotocopy3 = require("../assets/images/fotocopy3.jpg");
+const fotocopy4 = require("../assets/images/fotocopy4.jpeg");
+const fotocopy5 = require("../assets/images/fotocopy5.jpg");
+const fotocopy6 = require("../assets/images/fotocopy6.jpg");
+const berwarna = require("../assets/images/berwarna.jpg");
+const hitamputih = require("../assets/images/hitamputih.jpg");
+const jilid = require("../assets/images/jilid.jpg");
+
+export default {
+  avatar_1,
+  avatar_2,
+  avatar_3,
+  avatar_4,
+  avatar_5,
+
+  fotocopy1,
+  fotocopy2,
+  fotocopy3,
+  fotocopy4,
+  fotocopy5,
+  fotocopy6,
+
+  berwarna,
+  hitamputih,
+  jilid,
+};
